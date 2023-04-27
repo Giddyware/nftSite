7d0a59b019aef1317252cc02f0327a3def68fb45
Giddy <Idowugidiyanuoluwa@gmail.com>
2023-04-28 00:57:34 +0100

    Error: vite error

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import Button from "../Button";
+
+import MdOutlineCastConnected from "react-icons/md";
+import BiUserCircle from "react-icons/bi";
+
+// const Button_Details = () => {
+//   return (
+//     <Button>
+//       <BiUserCircle />
+//       Connect Wallet
+//       <div>
+//         <MdOutlineCastConnected />
+//       </div>
+//     </Button>
+//   );
+// };
+
+const Header = () => {
+  return (
+    <div>
+      {/* <Button_Details /> */}
+      kldfksf
+    </div>
+  );
+};
+export default Header;
