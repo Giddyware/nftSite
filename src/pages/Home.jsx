6b96a947d67c02ed41f57207216c9af48257cb38
Giddy <Idowugidiyanuoluwa@gmail.com>
2023-04-30 16:51:25 +0100

    fix: homepage error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,35 @@ import Categories from "../components/Category/Categories";
 import Header from "../components/Header/Header";
 
 const Home = () => {
+//   return (
+//     <div className="h-screen">
+//       <div
+//         className=""
+//         style={{
+//           backgroundImage: `url(${background})`,
+//           "backdrop-filter": "blur(5px)",
+//         }}
+//       >
+//         <Header />
+//         <Categories />
+//       </div>
+//       <Collection />
+//       <Cards />
+//     </div>
+//   );
+  const [count, setCount] = useState(0);
+  const [showModal, setShowmodal] = useState(false)
+
+  const ModalStatus = () => {
+    console.log("wroking")
+    setShowmodal((prev) => !prev)
+}
+
+<<<<<<< ezekiel
   return (
-    <div className="h-screen">
+    <div className="h-screen w-full relative">
+      <Overlay show={showModal} clear={ModalStatus}/>
+      <ConnectWallet show={showModal} modalStatus={ModalStatus} />
       <div
         className=""
         style={{
@@ -14,12 +41,14 @@ const Home = () => {
           "backdrop-filter": "blur(5px)",
         }}
       >
-        <Header />
+        <Header  showModal={ModalStatus}/>
         <Categories />
       </div>
       <Collection />
       <Cards />
     </div>
   );
+
+  
 };
 export default Home;
