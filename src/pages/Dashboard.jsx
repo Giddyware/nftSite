fff228ed1e94a4351c17d7726328e39870f6ecd0
Giddy <Idowugidiyanuoluwa@gmail.com>
2023-05-05 18:08:10 +0100

    feat: transaction table

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -56,14 +56,24 @@ const DashboardCard = () => {
   );
 };
 
-function createData(id, transaction, atm, date, status) {
+function createTransactionData(id, transaction, atm, date, status) {
   return { transaction, atm, date, status };
 }
 
-const rows = [
-  createData(1, "deposit", 5, 2, "completed"),
-  createData(2, "withdrawal", -4, 2, "pending"),
-  createData(3, "mint", -2, 5, "cancelled"),
+const transactionRows = [
+  createTransactionData(1, "deposit", 5, 2, "completed"),
+  createTransactionData(2, "withdrawal", -4, 2, "pending"),
+  createTransactionData(3, "mint", -2, 5, "cancelled"),
+];
+
+function createSalesData(id, transaction, nftName, from, to, atm, status) {
+  return { transaction, nftName, from, to, atm, status };
+}
+
+const SalesRows = [
+  createSalesData(1, "buy", "JK94ejk8", "zac", "Rob", -500, "completed"),
+  createSalesData(2, "sell", "jkcxs8jk4", "white", "Bill", 292, "pending"),
+  createSalesData(3, "mint", "jkds8r4k", "bell", "josh", -648, "cancelled"),
 ];
 
 const data = [
@@ -141,6 +151,59 @@ const data = [
   },
 ];
 
+const RecentSalesTable = () => {
+  return (
+    <TableContainer className="bg-[hsla(0,_0%,_20%,_1)] px-10 py-5 rounded-3xl">
+      <Table aria-label="dashboard-table" className="rounded">
+        <TableHead className="bg-black rounded ">
+          <TableRow className="rounded">
+            <TableCell className="text-white border-none">
+              Transaction
+            </TableCell>
+            <TableCell className="text-white border-none">NFT name</TableCell>
+            <TableCell className="text-white border-none">From</TableCell>
+            <TableCell className="text-white border-none">To</TableCell>
+            <TableCell className="text-white border-none">Amount</TableCell>
+            <TableCell className="text-white border-none">Status</TableCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody className="">
+          {SalesRows.map(
+            ({ id, transaction, nftName, from, to, atm, status }) => (
+              <TableRow className="" key={id}>
+                <TableCell className="text-white capitalize">
+                  {transaction}
+                </TableCell>
+                <TableCell className="text-white capitalize">
+                  {nftName}
+                </TableCell>
+                <TableCell className="text-white">{from}</TableCell>
+                <TableCell className="text-white">{to}</TableCell>
+                <TableCell className="text-white">{atm} ETH</TableCell>
+
+                <TableCell
+                  className={`capitalize ${
+                    status === "pending"
+                      ? "text-[hsla(28,_87%,_62%,_1)]"
+                      : status === "completed"
+                      ? "text-[hsla(84,_100%,_44%,_1)]"
+                      : status === "cancelled"
+                      ? "text-[hsla(0,_79%,_63%,_1)]"
+                      : "undefined"
+                  }`}
+                >
+                  {status}
+                </TableCell>
+              </TableRow>
+            )
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
 const DashboardTable = () => {
   return (
     <TableContainer className="bg-[hsla(0,_0%,_20%,_1)] px-10 py-5 rounded-3xl">
@@ -157,7 +220,7 @@ const DashboardTable = () => {
         </TableHead>
 
         <TableBody className="">
-          {rows.map(({ id, transaction, atm, date, status }) => (
+          {transactionRows.map(({ id, transaction, atm, date, status }) => (
             <TableRow className="" key={id}>
               <TableCell className="text-white capitalize">
                 {transaction === "withdrawal" ? (
@@ -319,6 +382,13 @@ const Dashboard = () => {
             {data.map((el) => (
               <Card imageWidth={56} key={el.id} {...el} />
             ))}
+          </div>
+        </div>
+
+        <div className="mt-12 text-xs font-bold">
+          <p className="mb-7">Recent Sales</p>
+          <div>
+            <RecentSalesTable />
           </div>
         </div>
       </div>
