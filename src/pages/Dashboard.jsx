c0aa1cc6c0c7a582a062a2943f3fb0a590db96ee
Giddy <Idowugidiyanuoluwa@gmail.com>
2023-05-05 12:40:47 +0100

    feat: added dashboardTable, coinList and others

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,6 +4,16 @@ import { MdCreate, MdOutlineCollections, MdSummarize } from "react-icons/md";
 import { GiReceiveMoney, GiTakeMyMoney } from "react-icons/gi";
 import { GrTransaction } from "react-icons/gr";
 import { BiChevronUp, BiSupport } from "react-icons/bi";
+import { BsArrowDownLeft, BsArrowUpRight } from "react-icons/bs";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+
 import avatar from "../assets/avatar.png";
 
 const DashboardLinks = ({ name, icon }) => {
@@ -29,6 +39,72 @@ const DashboardCard = () => {
         </button>
       </div>
     </div>
+  );
+};
+
+function createData(id, transaction, atm, date, status) {
+  return { transaction, atm, date, status };
+}
+
+const rows = [
+  createData(1, "deposit", 5, 2, "completed"),
+  createData(2, "withdrawal", -4, 2, "pending"),
+  createData(3, "mint", -2, 5, "cancelled"),
+];
+
+const DashboardTable = () => {
+  return (
+    <TableContainer className="bg-[hsla(0,_0%,_20%,_1)] px-10 py-5 rounded-3xl">
+      <Table aria-label="dashboard-table" className="rounded">
+        <TableHead className="bg-black rounded ">
+          <TableRow className="rounded">
+            <TableCell className="text-white border-none">
+              Transaction
+            </TableCell>
+            <TableCell className="text-white border-none">Amount</TableCell>
+            <TableCell className="text-white border-none">Date</TableCell>
+            <TableCell className="text-white border-none">Status</TableCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody className="">
+          {rows.map(({ id, transaction, atm, date, status }) => (
+            <TableRow className="" key={id}>
+              <TableCell className="text-white capitalize">
+                {transaction === "withdrawal" ? (
+                  <div className="flex gap-x-3">
+                    <BsArrowUpRight color="hsla(0, 79%, 63%, 1)" />
+                    {transaction}
+                  </div>
+                ) : transaction === "deposit" || transaction === "mint" ? (
+                  <div className="flex gap-x-3">
+                    <BsArrowDownLeft color="hsla(84, 100%, 44%, 1)" />
+                    {transaction}
+                  </div>
+                ) : (
+                  "undefined"
+                )}
+              </TableCell>
+              <TableCell className="text-white">{atm}</TableCell>
+              <TableCell className="text-white">{date}</TableCell>
+              <TableCell
+                className={`capitalize ${
+                  status === "pending"
+                    ? "text-[hsla(28,_87%,_62%,_1)]"
+                    : status === "completed"
+                    ? "text-[hsla(84,_100%,_44%,_1)]"
+                    : status === "cancelled"
+                    ? "text-[hsla(0,_79%,_63%,_1)]"
+                    : "undefined"
+                }`}
+              >
+                {status}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
@@ -116,9 +192,41 @@ const Dashboard = () => {
         </div>
 
         <DashboardCard />
-        <div>
-          <p>Profile</p>
+        <div className="flex flex-col gap-5 my-10 mx-3">
+          <div className="w-full border-b pb-4">
+            <div className="flex justify-between">
+              {/* TODO: Add the logo of the coin */}
+              <p>logo ETH</p>
+              <p>~19400</p>
+            </div>
+            <div className="flex justify-between text-gray-500">
+              {/* TODO: Add the logo of the coin */}
+              <p>Logo WETH</p>
+              <p>~19400</p>
+            </div>
+          </div>
+          <div className="w-full ">
+            <div className="flex justify-between">
+              {/* TODO: Add the logo of the coin */}
+              <p>logo ETH</p>
+              <p>~19400</p>
+            </div>
+            <div className="flex justify-between text-gray-500">
+              {/* TODO: Add the logo of the coin */}
+              <p>Logo WETH</p>
+              <p>~19400</p>
+            </div>
+          </div>
         </div>
+        <div className="mt-12 ">
+          <p className="mb-7">Recent Transactions</p>
+          <div>
+            <DashboardTable />
+          </div>
+        </div>
+
+
+        
       </div>
     </div>
   );
