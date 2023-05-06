59f0d002a82796c8720c76fb22bc2f2dfaebf341
ayoeze191 <ayoeze191@gmail.com>
2023-05-06 14:55:44 +0100

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+// import "./App.css";
 import Header from "./components/Header/Header";
 import background from "./assets/background.jpg";
 import Cards from "./components/Cards/Cards";
