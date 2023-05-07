de7450eab682158ddb8bc8df9a05fb8fa133c228
Giddy <Idowugidiyanuoluwa@gmail.com>
2023-05-07 14:22:48 +0100

    fix: some bug

--- src/components/UI/SideNav.jsx
+++ src/components/UI/SideNav.jsx
@@ -1,10 +1,11 @@
 import { FaMoneyCheck } from "react-icons/fa";
-import { FcWiFiLogo } from "react-icons/fc";
+
 import { MdCreate, MdOutlineCollections, MdSummarize } from "react-icons/md";
 import { GiReceiveMoney, GiTakeMyMoney } from "react-icons/gi";
 import { GrTransaction } from "react-icons/gr";
 import { BiSupport } from "react-icons/bi";
 import { Link } from "react-router-dom";
+import Logo from '../../assets/logo.png'
 
 const DashboardLinks = ({ hash, name, icon }) => {
   console.log(hash);
@@ -27,14 +28,14 @@ const handleScroll = (ref) => {
 const SideNav = ({ refs }) => {
   return (
     <div className="">
-      {/* TODO: Make the Sidebar component */}
+      {/* TODO: Make the SideNav component */}
       <div className="bg-gray-100 min-h-full py-8">
         <a
           href="#"
           className="flex items-center justify-center p-2 pb-8 border-b"
         >
-          <FcWiFiLogo size={"10%"} />
-          <h1 className="text-2xl font-bold capitalize">OPENSEA</h1>
+          <img className="w-80" src={Logo} alt="logo" />
+          {/* <h1 className="text-2xl font-bold capitalize">OPENSEA</h1> */}
         </a>
         <ul className="my-10 ">
           <li className="px-5 py-6 ml-20">
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -192,7 +192,7 @@ const Dashboard = () => {
 
         {/* </div> */}
         <div
-          className="mx- my-10 flex flex-col bg-gray-200 px-4 py-7 gap-4 rounded-xl"
+          className="mx- my-10 flex flex-col bg-gray-200 px-8 py-7 gap-4 rounded-xl"
           id="listing"
         >
           <div className="grid grid-cols-[40px,_80px_1fr] w-full items-center">
