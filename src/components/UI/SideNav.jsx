b594d93af52d95c366e7b90d801c000d211a2770
Giddy <Idowugidiyanuoluwa@gmail.com>
2023-05-09 18:36:01 +0100

    fix: font-size and hover in the dashboard

--- src/components/UI/SideNav.jsx
+++ src/components/UI/SideNav.jsx
@@ -9,9 +9,9 @@ import Logo from "../../assets/logo.png";
 
 const DashboardLinks = ({ hash, name, icon }) => {
   return (
-    <div className="flex items-center justify-start pl-5 rounded-l-full cursor-pointer hover:bg-gray-400">
+    <div className="flex items-center justify-start">
       {icon}
-      <p className="p-5 text-2xl">{name}</p>
+      <p className="p-2 text-2xl">{name}</p>
     </div>
   );
 };
@@ -31,20 +31,20 @@ const SideNav = ({ refs }) => {
       <div className="min-h-full py-8 bg-gray-100">
         <a
           href="#"
-          className="flex items-center justify-center p-2 pb-8 border-b"
+          className="flex items-center justify-center p-2 pb-8 border-b "
         >
           <img className="w-80" src={Logo} alt="logo" />
           {/* <h1 className="text-2xl font-bold capitalize">OPENSEA</h1> */}
         </a>
-        <ul className="my-10 ">
-          <li className="px-5 py-6 ml-20">
+        <ul className="flex flex-col gap-5 my-10">
+          <li className="px-5 py-6 ml-20 rounded-l-full cursor-pointer hover:bg-gray-400">
             <DashboardLinks
               icon={<MdSummarize size={"10%"} />}
               name={"Account Summary"}
             />
           </li>
           <li
-            className="px-5 py-6 ml-20"
+            className="px-5 py-6 ml-20 rounded-l-full cursor-pointer hover:bg-gray-400"
             onClick={() => {
               console.log(refs.nftRef, "nftRef");
               refs.nftRef.current.scrollIntoView();
@@ -55,7 +55,7 @@ const SideNav = ({ refs }) => {
               name={"NFT Collection"}
             />
           </li>
-          <li className="px-5 py-6 ml-20">
+          <li className="px-5 py-6 ml-20 rounded-l-full cursor-pointer hover:bg-gray-400">
             <DashboardLinks
               icon={<MdCreate size={"10%"} />}
               name={"Mint / Create"}
@@ -63,7 +63,7 @@ const SideNav = ({ refs }) => {
             />
           </li>
           <li
-            className="px-5 py-6 ml-20"
+            className="px-5 py-6 ml-20 rounded-l-full cursor-pointer hover:bg-gray-400"
             onClick={() => {
               console.log(refs.transactionRef, "transactionRef");
               refs.transactionRef.current.scrollIntoView();
@@ -76,7 +76,7 @@ const SideNav = ({ refs }) => {
           </li>
 
           <li
-            className="px-5 py-6 ml-20"
+            className="px-5 py-6 ml-20 rounded-l-full cursor-pointer hover:bg-gray-400"
             onClick={() => {
               console.log(refs.saleRef, "salesRef");
               refs.saleRef.current.scrollIntoView();
@@ -90,13 +90,13 @@ const SideNav = ({ refs }) => {
             />
             {/* </Link> */}
           </li>
-          <li className="px-5 py-6 ml-20">
+          <li className="px-5 py-6 ml-20 rounded-l-full cursor-pointer hover:bg-gray-400">
             <DashboardLinks
               icon={<BiSupport size={"10%"} />}
               name={"Support"}
             />
           </li>
-          <li className="px-5 py-6 ml-20">
+          <li className="px-5 py-6 ml-20 rounded-l-full cursor-pointer hover:bg-gray-400">
             <DashboardLinks
               icon={<GiReceiveMoney size={"10%"} />}
               name={"Withdraw / Deposit"}
