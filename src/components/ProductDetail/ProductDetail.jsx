26a1722e6641bc8fb7418725fd0303a6e3b44405
Giddy <Idowugidiyanuoluwa@gmail.com>
2023-05-24 16:45:41 +0100

    feat: Worked on the loading animations

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -103,8 +103,10 @@ const ProductDetail = () => {
 
   if (!selectedItem) {
     return (
-      <div>
-        <img src={Logo} alt="" />
+      <div className="flex justify-center items-center min-h-full min-w-full">
+        <div className="w-56 h-28 loader">
+          <img className="ml-12" src={Logo} alt="" />
+        </div>
       </div>
     );
   }
